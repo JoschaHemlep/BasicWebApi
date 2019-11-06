5ee8dee2a6b42c97a6018aa68a0d04983f1661fe
Joscha <JoschaHemlep@gmail.com>
2019-11-06 22:49:41 +0100

    ef core added

---
+++ DbAccess/DatabaseContext.cs
@@ -0,0 +1,13 @@
+﻿using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.CodeAnalysis;
+
+namespace DbAccess
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext([NotNull] DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
---
+++ DbAccess/DbContextFactory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DbAccess
+{
+    public class DbContextFactory
+    {
+        private readonly DbContextOptions options;
+
+        public DbContextFactory(DbContextOptions options)
+        {
+            this.options = options;
+        }
+
+        public DatabaseContext Create()
+        {
+            return new DatabaseContext(options);
+        }
+
+    }
+}
